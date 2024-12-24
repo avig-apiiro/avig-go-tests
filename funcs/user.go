4858ca05479e73c6640df6a1a3bcb85dc8665afc
avig-apiiro <avi.glatstein@apiiro.com>
2024-12-24 14:53:24 +0200

    web go project

--- funcs/user.go
+++ funcs/user.go
@@ -1,10 +1,12 @@
 package funcs
 
 type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Cvv     string `json:"cvv"`
+	Address string `json:"address"`
 }
 
 func GetUser(name string) *User {
-	return &User{Name: name, Email: "jhodndoe@example.com"}
+	return &User{Name: name, Email: "jhodndoe@example.com", Address: "home", Cvv: "012"}
 }

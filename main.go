775e2b102c5a1c8af020419433f9b2b16d0ed16f
avig-apiiro <avi.glatstein@apiiro.com>
2024-12-24 13:32:50 +0200

    web go project

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Hello world")
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/endpoint", getHello)
+	//http.HandleFunc("/endpoint", getHello)
 	http.HandleFunc("/user", funcs.UserHandler)
 	//http.HandleFunc(ENDPOINT_URI, getHello)
-	//http.HandleFunc(ENDPOINT_URI_BASE+ENDPOINT_URI, funcs.GetOtherHello)
+	http.HandleFunc(ENDPOINT_URI_BASE+ENDPOINT_URI, funcs.GetOtherHello)
 
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {

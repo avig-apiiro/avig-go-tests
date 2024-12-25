257fb8a914059c4861d2e34d76daecd3e97eabcc
avig-apiiro <avi.glatstein@apiiro.com>
2024-12-25 14:06:31 +0200

    web go project

--- apis/apis.go
+++ apis/apis.go
@@ -8,11 +8,12 @@ import (
 
 func UserHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the "name" query parameter from the request
-	aname := r.URL.Query().Get("name")
+	name := r.URL.Query().Get("name")
 
 	// Mock user data
 	user := funcs.User{}
-	funcs.GetUser(aname, &user)
+	funcs.GetUser(name, &user)
+	user.Cvv = "111"
 	// Set response content type to JSON
 	w.Header().Set("Content-Type", "application/json")
 

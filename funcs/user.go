478f699b60921a728d2009f6aa515215c5c83a62
avig-apiiro <avi.glatstein@apiiro.com>
2025-01-01 20:00:40 +0200

    ndent

--- funcs/user.go
+++ funcs/user.go
@@ -1,21 +1,21 @@
 package funcs
 
 type User struct {
-	UserName       string `json:"name"`
-	Email          string `json:"email"`
-	Cvv            string `json:"cvv"`
-	Address        string `json:"address"`
-	OwnerName      string `json:"owner_name"`
-	PatientAddress string `json:"patient_address"`
+    UserName       string `json:"name"`
+    Email          string `json:"email"`
+    Cvv            string `json:"cvv"`
+    Address        string `json:"address"`
+    OwnerName      string `json:"owner_name"`
+    PatientAddress string `json:"patient_address"`
 }
 
 func GetUser(name string) *User {
-	user := User{}
-	user.UserName = name
-	user.Email = "jhodndo@example.com"
-	user.Address = "home"
-	user.Cvv = "012"
-	user.PatientAddress = "home"
-	user.OwnerName = "jhodndo"
-	return &user
+    user := User{}
+    user.UserName = name
+    user.Email = "jhodndo@example.com"
+    user.Address = "home"
+    user.Cvv = "012"
+    user.PatientAddress = "home"
+    user.OwnerName = "jhodndo"
+    return &user
 }

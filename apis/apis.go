4a8bb54453dac14ea26d0b40dad19f91aaab8135
avig-apiiro <avi.glatstein@apiiro.com>
2025-01-02 08:13:28 +0200

    add alias

--- apis/apis.go
+++ apis/apis.go
@@ -1,30 +1,30 @@
 package apis
 
 import (
-    "avig-go-tests/funcs"
-    "encoding/json"
-    "net/http"
+	"avig-go-tests/funcs"
+	"encoding/json"
+	. "net/http"
 )
 
 type UserApi struct{}
 
-func (userApi UserApi) UserHandler(w http.ResponseWriter, r *http.Request) {
-    // Get the "name" query parameter from the request
-    name := r.URL.Query().Get("name")
+func (userApi UserApi) UserHandler(w ResponseWriter, r *Request) {
+	// Get the "name" query parameter from the request
+	name := r.URL.Query().Get("name")
 
-    // Mock user data
+	// Mock user data
 
-    user := funcs.GetUser(name)
-    // Set response content type to JSON
-    w.Header().Set("Content-Type", "application/json")
+	user := funcs.GetUser(name)
+	// Set response content type to JSON
+	w.Header().Set("Content-Type", "application/json")
 
-    // Serialize user to JSON and write to response
-    if err := json.NewEncoder(w).Encode(user); err != nil {
-        http.Error(w, "Failed to encode user", http.StatusInternalServerError)
-        return
-    }
+	// Serialize user to JSON and write to response
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		Error(w, "Failed to encode user", StatusInternalServerError)
+		return
+	}
 }
 
 func (userApi UserApi) Serve() {
-    http.HandleFunc("/api2", userApi.UserHandler)
+	HandleFunc("/api2", userApi.UserHandler)
 }

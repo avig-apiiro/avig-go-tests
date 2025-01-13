3c169b14d1ed0daea4bb620dfb4e6884dfac2be8
avig-apiiro <avi.glatstein@apiiro.com>
2025-01-13 16:41:45 +0200

    wip

--- project2/main.go
+++ project2/main.go
@@ -7,25 +7,17 @@ import (
 func main() {
 	app := iris.Default()
 
-	app.Get("/user/{name}", func(ctx iris.Context) {
+	app.Get("/V3/user/{name}", func(ctx iris.Context) {
 		name := ctx.Params().Get("name")
 		ctx.Writef("Hello %s", name)
 	})
 	v2 := app.Party("/v2")
 	{
-		v2.Post("/login", loginEndpoint)
-		v2.Put("/submit", submitEndpoint)
-		v2.Head("/read", readEndpoint)
+		v2.Post("/V3/login", loginEndpoint)
 	}
 	app.Listen(":8081")
 }
 
 func loginEndpoint(ctx iris.Context) {
-	ctx.StatusCode(iris.StatusCreated)
-}
-func submitEndpoint(ctx iris.Context) {
-	ctx.StatusCode(iris.StatusCreated)
-}
-func readEndpoint(ctx iris.Context) {
 	ctx.StatusCode(iris.StatusCreated)
 }

832c1d9755de11502a9181b5161f8fd5612bdcf5
avig-apiiro <avi.glatstein@apiiro.com>
2025-01-13 16:39:27 +0200

    chi

---
+++ project2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	app := iris.Default()
+
+	app.Get("/user/{name}", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		ctx.Writef("Hello %s", name)
+	})
+	v2 := app.Party("/v2")
+	{
+		v2.Post("/login", loginEndpoint)
+		v2.Put("/submit", submitEndpoint)
+		v2.Head("/read", readEndpoint)
+	}
+	app.Listen(":8081")
+}
+
+func loginEndpoint(ctx iris.Context) {
+	ctx.StatusCode(iris.StatusCreated)
+}
+func submitEndpoint(ctx iris.Context) {
+	ctx.StatusCode(iris.StatusCreated)
+}
+func readEndpoint(ctx iris.Context) {
+	ctx.StatusCode(iris.StatusCreated)
+}

06ba673aec3fffee3beca69f6e319c67ba959e2a
avig-apiiro <avi.glatstein@apiiro.com>
2025-01-14 13:27:44 +0200

    wip

--- gorilla.go
+++ gorilla.go
@@ -14,13 +14,21 @@ func (a apiEvents) ServeHTTP(writer http.ResponseWriter, request *http.Request) 
 	panic("implement me")
 }
 
+func (a apiEvents) fetchConfiguration() func(http.ResponseWriter, *http.Request) {
+	panic("implement me")
+}
+
+func (a apiEvents) updateConfiguration() func(http.ResponseWriter, *http.Request) {
+	panic("implement me")
+}
+
 func gorillamain() {
 	r := mux.NewRouter()
 	var s, s2 = r.PathPrefix("/products").Subrouter(), r.PathPrefix("/products2").Subrouter()
 	s.HandleFunc("/", ProductsHandler).Methods("GET", "POST")                                          // "/products/"
 	s2.HandleFunc("/{key}", ProductHandler).Methods("GET").Queries("filter", "{filter}", "by", "{by}") // "/products/{key}/"
-	r.Handle("/ids/{id}/events", &apiEvents{auth: auth}).Methods("GET")
-
+	r.Handle("/ids/{id}/events", &apiEvents{auth: "auth"}).Methods("GET")
+	uc := &apiEvents{}
 	r.HandleFunc("/configuration", uc.fetchConfiguration()).Methods("GET")
 	r.HandleFunc("/configuration", uc.updateConfiguration()).Methods("POST")
 }

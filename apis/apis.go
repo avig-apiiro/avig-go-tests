e42367886f345cb49ba9c724f5c181e0eb839948
avig-apiiro <avi.glatstein@apiiro.com>
2025-04-10 16:55:58 +0300

    wip

--- apis/apis.go
+++ apis/apis.go
@@ -16,6 +16,23 @@ func (MyUser UserApi) UserHandler(w ResponseWriter, r *Request) {
 	//user := funcs.GetUser(name)
 	user := funcs.User{}
 	user.UserName = name
+	user.Email = "https://api.accuweather.com/v2/test"
+	// Set response content type to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		Error(w, "Failed to  encode user", StatusInternalServerError)
+		return
+	}
+
+}
+func (MyUser UserApi) SecondUserHandler(w ResponseWriter, r *Request) {
+	// Get the "name" query parameter from the request
+	name := r.URL.Query().Get("name")
+
+	//user := funcs.GetUser(name)
+	user := funcs.User{}
+	user.UserName = name
 	user.Email = "https://api.accuweather.com/v1/test"
 	// Set response content type to JSON
 	w.Header().Set("Content-Type", "application/json")
@@ -26,7 +43,6 @@ func (MyUser UserApi) UserHandler(w ResponseWriter, r *Request) {
 	}
 
 }
-
 func (userApi UserApi) Serve() {
 	HandleFunc("/api6", userApi.UserHandler)
 }
